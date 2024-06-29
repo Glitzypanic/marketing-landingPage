022cb995ae8add4c7192c092dcc5d86c374b48bd
glitzypanic <jose.fariasdv@gmail.com>
2024-06-29 12:56:04 -0700

    refactor: Update Partners component and styles

--- src/Components/Partners.jsx
+++ src/Components/Partners.jsx
@@ -2,17 +2,17 @@ import "../Styles/Components/Partners.css";
 
 const Partners = () => {
   return (
-    <article className="partners-container">
-      <h2 className="partners-title">Companies We Work With</h2>
-      <section className="partners-container">
-        <div className="partners-top">
+    <article className="client-showcase">
+      <h2 className="client-showcase__title">Companies We Work With</h2>
+      <section className="client-showcase__grid">
+        <div className="client-showcase__row-top">
           <img src="/svg/Spotify.svg" alt="Spotify Logo" />
           <img src="/svg/Google.svg" alt="Google Logo" />
           <img src="/svg/uber.svg" alt="Uber Logo" />
           <img src="/svg/Microsoft.svg" alt="Microsoft Logo" />
           <img src="/svg/Shopify.svg" alt="Shopify Logo" />
         </div>
-        <div className="partners-bottom">
+        <div className="client-showcase__row-bottom">
           <img src="/svg/Evernote.svg" alt="Evernote Logo" />
           <img src="/svg/Adobe.svg" alt="Adobe Logo" />
           <img src="/svg/Paypal.svg" alt="Paypal Logo" />
