71a8c54af8baab41e7c417b4b4f60f3e8e79ede7
glitzypanic <jose.fariasdv@gmail.com>
2024-06-29 12:54:30 -0700

    refactor: Update NavBar component and styles

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,15 +2,20 @@ import "../Styles/Components/NavBar.css";
 
 const NavBar = () => {
   return (
-    <nav className="nav-container">
-      <img className="nav-logo" src="/Logo.png" alt="Logo" />
-      <ul className="nav-links">
-        <li>How it Works</li>
-        <li> Our Work</li>
-        <li>Pricing</li>
-        <li>About Us</li>
-      </ul>
-      <button className="nav-button">Book A Call</button>
+    <nav className="main-nav">
+      <div className="main-nav__container">
+        <img className="main-nav__logo" src="/Logo.png" alt="Company Logo" />
+        <ul className="main-nav__menu">
+          <li>How it Works</li>
+          <li> Our Work</li>
+          <li>Pricing</li>
+          <li>About Us</li>
+        </ul>
+        <button className="main-nav__cta">Book A Call</button>
+      </div>
+      <div className="main-nav__line">
+        <span></span>
+      </div>
     </nav>
   );
 };
